752ba2c89bb45bf6a8ef18ec3cbe142ab65f0e17
AndreeaMagda <andreea2002@gmail.com>
2025-10-30 18:37:04 +0200

    Created the Main Menu

--- app/src/main/kotlin/Main.kt
+++ app/src/main/kotlin/Main.kt
@@ -1,3 +1,32 @@
 fun main(){
-    println("hamster")
+    println("=======================================")
+    println("    HAMSTER FOOD DISPENSER SYSTEM")
+    println("=======================================")
+
+    
+    var menu= arrayOf("1. Managae Hamster","2. Manage Food Inventory","3. Feeding Operations","4. Reports","0. Exit")
+    println(menu[0])
+    println(menu[1])
+    println(menu[2])
+    println(menu[3])
+    println(menu[4])
+    
+    print("Enter choice: ")
+    val choice = readln().toInt()
+
+    if (choice == 1) {
+        println("You choose: "+menu[0])
+    } else if(choice==2){
+         println("You choose: "+menu[1])
+    }
+    else if(choice==3){
+         println("You choose: "+menu[2])
+    }else if(choice==3){
+         println("You choose: "+menu[3])
+    }else if(choice==4){
+         println("You choose: "+menu[4])
+    }
+
+
+   
 }
